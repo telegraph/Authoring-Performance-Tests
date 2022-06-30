ad60ec9c46311ed5cba6989937097fc42beb3779
BenMacDonagh <ben.macdonagh@telegraph.co.uk>
2022-06-30 10:54:13 +0100

    Update Jenkinsfile.nightly.groovy

--- Jenkinsfile.nightly.groovy
+++ Jenkinsfile.nightly.groovy
@@ -1,14 +1,23 @@
 pipeline {
     agent any
+
+    tools {
+        jdk 'openjdk-11'
+    }
+
+    environment {
+        MVNW_ALIAS = "./mvnw --no-transfer-progress"
+    }
+
     stages {
         stage("Build Maven") {
             steps {
-                sh 'mvn -B clean package'
+                sh '$MVNW_ALIAS clean'
             }
         }
         stage("Run Gatling") {
             steps {
-                sh 'mvn gatling:test'
+                sh '$MVNW_ALIAS gatling:test'
             }
             post {
                 always {
